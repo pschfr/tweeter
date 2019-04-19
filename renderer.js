3422a6d77b70c53231d676479bf36c28ae4cd057
Paul Schaefer <yeppaul2@gmail.com>
2019-04-19 16:19:06 -0400

    Added dotenv and twitter dependencies!

--- renderer.js
+++ renderer.js
@@ -1,3 +1,13 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+const dotenv = require('dotenv').config()
+const Twitter = require('twitter')
+
+const client = new Twitter({
+	consumer_key: process.env.TWITTER_CONSUMER_KEY,
+	consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+	access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+	access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+})
